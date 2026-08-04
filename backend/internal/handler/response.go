463d0941fbd6d5071aa534d5931635189fbdbdd3
Matvey Galaktionov <galaktionov_matvey@mail.ru>
2026-08-04 14:00:11 +0300

    feat(auth): auth handlers

---
+++ backend/internal/handler/response.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"avito-antifraud-trainer/internal/dto"
+	"encoding/json"
+	"net/http"
+)
+
+const (
+	CodeInvalidJson   = "INVALID JSON"
+	CodeInternalError = "INTERNAL ERROR"
+)
+
+const (
+	MessageInvalidJson   = "некорректный формат данных"
+	MessageInternalError = "внутренняя ошибка сервера"
+)
+
+func writeError(w http.ResponseWriter, status int, code string, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	err := dto.Error{Code: code, Message: message}
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(err)
+}
+
+func writeResponse(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
