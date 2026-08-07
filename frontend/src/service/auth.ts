ae60e6758712e61422fda6ce87164fa708921c8d
melon4521 <vlad123kyz@gmail.com>
2026-08-07 12:38:50 +0300

    refactor(service): imports and code simplify in auth service

--- frontend/src/service/auth.ts
+++ frontend/src/service/auth.ts
@@ -1,44 +1,12 @@
-import { isUser, UserResponse, type AuthUser, type User } from '@/types';
-import axios, { AxiosHeaders } from 'axios';
-
-const api = axios.create({
-  baseURL: '/api',
-});
-
-api.interceptors.request.use(config => {
-  const authStorage = localStorage.getItem('auth-storage');
-
-  if (!authStorage) {
-    return config;
-  }
-
-  const parsed = JSON.parse(authStorage) as {
-    state: {
-      user: User;
-    };
-  };
-
-  if (isUser(parsed.state.user)) {
-    const token = parsed.state.user.token;
-
-    if (token) {
-      const headers = AxiosHeaders.from(config.headers ?? {});
-      headers.set('Authorization', `Bearer ${token}`);
-      config.headers = headers;
-    }
-  }
-
-  return config;
-});
+import { UserResponse, type AuthUser } from '@/types/user';
+import api from '@/service/api';
 
 export async function registerUser(data: AuthUser): Promise<UserResponse> {
   const response = await api.post('/auth/register', data);
-  const userData: UserResponse = UserResponse.parse(response.data);
-  return userData;
+  return UserResponse.parse(response.data);
 }
 
 export async function loginUser(data: AuthUser): Promise<UserResponse> {
   const response = await api.post('/auth/login', data);
-  const userData: UserResponse = UserResponse.parse(response.data);
-  return userData;
+  return UserResponse.parse(response.data);
 }
