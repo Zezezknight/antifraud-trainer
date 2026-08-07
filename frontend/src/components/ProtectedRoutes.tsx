af18951edbb4af33fa478d3373ed0fb9990588c4
melon4521 <vlad123kyz@gmail.com>
2026-08-07 13:32:44 +0300

    feat(components): add token expire check in ProtectedRoutes with user redirect to /login

--- frontend/src/components/ProtectedRoutes.tsx
+++ frontend/src/components/ProtectedRoutes.tsx
@@ -1,13 +1,24 @@
 import { useAuth } from '@/hooks/auth';
 import HydrateFallbackPage from '@/pages/HydrateFallback';
+import { clearUser } from '@/store/user';
+import { isTokenExpired } from '@/utils/auth';
+import { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router';
 
 function ProtectedRoutes() {
-  const { isAuth, isHydrated } = useAuth();
+  const { user, isAuth, isHydrated } = useAuth();
+  const token = user?.token ?? null;
+  const isExpired = token ? isTokenExpired(token) : false;
+
+  useEffect(() => {
+    if (token && isExpired) {
+      clearUser();
+    }
+  }, [token, isExpired]);
 
   if (!isHydrated) return <HydrateFallbackPage />;
 
-  return isAuth ? <Outlet /> : <Navigate to="/login" replace />;
+  return !isAuth || isExpired ? <Navigate to="/login" replace /> : <Outlet />;
 }
 
 export default ProtectedRoutes;
