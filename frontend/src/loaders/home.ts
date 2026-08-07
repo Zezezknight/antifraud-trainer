816f14fc74eba4d610d96cebad293771f3429ab2
melon4521 <vlad123kyz@gmail.com>
2026-08-08 00:31:40 +0300

    feat(loaders): add profile loader into home loader

--- frontend/src/loaders/home.ts
+++ frontend/src/loaders/home.ts
@@ -1,39 +1,67 @@
+import { getUserProfile } from '@/service/profile';
 import { getScenarios } from '@/service/scenarios';
+import { useUserProfileStore } from '@/store/profile';
 import { useScenariosStore } from '@/store/scenarios';
+import type { UserProfile } from '@/types/profile';
 import type { Scenario } from '@/types/scenarios';
 
 export interface HomeLoader {
+  profile: UserProfile;
   seller: Scenario<'seller'>[];
   buyer: Scenario<'buyer'>[];
 }
 
 export async function homeLoader() {
+  const [profile, scenarios] = await Promise.all([
+    userProfileLoader(),
+    scenariosLoader(),
+  ]);
+
+  return { profile, ...scenarios };
+}
+
+async function scenariosLoader(): Promise<{
+  buyer: Scenario<'buyer'>[];
+  seller: Scenario<'seller'>[];
+}> {
   try {
     const { scenarios, setScenarios } = useScenariosStore.getState();
 
-    const buyerPromise =
-      scenarios.buyer.length === 0
-        ? getScenarios<'buyer'>('buyer').then(scenarios => {
-            setScenarios('buyer', scenarios);
-            return scenarios;
-          })
-        : Promise.resolve(scenarios.buyer);
+    const [buyer, seller] = await Promise.all([
+      scenarios.buyer.length > 0
+        ? scenarios.buyer
+        : getScenarios<'buyer'>('buyer').then(items => {
+            setScenarios('buyer', items);
+            return items;
+          }),
+      scenarios.seller.length > 0
+        ? scenarios.seller
+        : getScenarios<'seller'>('seller').then(items => {
+            setScenarios('seller', items);
+            return items;
+          }),
+    ]);
 
-    const sellerPromise =
-      scenarios.seller.length === 0
-        ? getScenarios<'seller'>('seller').then(scenarios => {
-            setScenarios('seller', scenarios);
-            return scenarios;
-          })
-        : Promise.resolve(scenarios.seller);
-
-    const [buyer, seller] = await Promise.all([buyerPromise, sellerPromise]);
     return { buyer, seller };
   } catch (error) {
-    console.error(error);
-    return {
-      buyer: [],
-      seller: [],
-    };
+    console.error('Ошибка загрузки сценариев:', error);
+    return { buyer: [], seller: [] };
+  }
+}
+
+async function userProfileLoader(): Promise<UserProfile> {
+  const { profile, setProfile } = useUserProfileStore.getState();
+
+  if (profile !== null) {
+    return profile;
+  }
+
+  try {
+    const userProfile = await getUserProfile();
+    setProfile(userProfile);
+    return userProfile;
+  } catch (error) {
+    console.error('Ошибка загрузки профиля:', error);
+    throw error;
   }
 }
