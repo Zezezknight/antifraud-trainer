9b29d3d375d7a73d0aa5ae907f067d63d1765b5c
melon4521 <vlad123kyz@gmail.com>
2026-08-07 17:51:30 +0300

    refactor(service): make generic function by scenario role

--- frontend/src/service/scenarios.ts
+++ frontend/src/service/scenarios.ts
@@ -1,8 +1,10 @@
 import api from '@/service/api';
 import * as z from 'zod';
-import { ScenarioSchema, type Scenario } from '@/types/scenarios';
+import { ScenarioSchema, type Role, type Scenario } from '@/types/scenarios';
 
-export async function getScenarios(): Promise<Scenario[]> {
-  const response = await api.get('/scenarios');
-  return z.array(ScenarioSchema).parse(response.data);
+export async function getScenarios<T extends Role>(
+  role: Role,
+): Promise<Scenario<T>[]> {
+  const response = await api.get(`/scenarios?role=${role}`);
+  return z.array(ScenarioSchema).parse(response.data) as Scenario<T>[];
 }
