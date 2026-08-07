b9f99422d490b2a1b2b931714b8640d209706ec7
melon4521 <vlad123kyz@gmail.com>
2026-08-07 14:11:52 +0300

    feat(pages): Home page with NavigationBar and tabs stub

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,33 @@
+import NavigationBar from '@/components/NavigationBar';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <NavigationBar />
+      <div className="container mx-auto px-8 flex flex-col gap-4 sm:gap-8">
+        <h1 className="text-3xl sm:text-4xl font-bold">Выберите сценарий</h1>
+        <p className="text-md sm:text-lg md:w-3/4 lg:w-1/2">
+          Пройдите переписку от лица продавца или покупателя. Зона риска заранее
+          не раскрывается — будьте внимательны в моменте.
+        </p>
+      </div>
+      <div className="container mx-auto px-8">
+        <Tabs defaultValue="seller">
+          <TabsList className="w-full flex items-center bg-muted border-2 border-border mb-8">
+            <TabsTrigger className="cursor-pointer" value="seller">
+              Продавец
+            </TabsTrigger>
+            <TabsTrigger className="cursor-pointer" value="buery">
+              Покупатель
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="seller">Сценарии для "Продавец"</TabsContent>
+          <TabsContent value="buery">Сценарии для "Покупатель"</TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 }
 
 export default Home;
