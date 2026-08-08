fb8e43dc2d0a97f47d70f858d38f87db2b920d69
melon4521 <vlad123kyz@gmail.com>
2026-08-08 11:48:42 +0300

    refactor&feat(loaders): reorgonize loaders by adding utils functions in utils.ts, shared loaders in loaders.ts and page loader functions in corresponding files; add profile page loader

--- frontend/src/loaders/home.ts
+++ frontend/src/loaders/home.ts
@@ -1,9 +1,8 @@
-import { getUserProfile } from '@/service/profile';
-import { getScenarios } from '@/service/scenarios';
-import { useUserProfileStore } from '@/store/profile';
-import { useScenariosStore } from '@/store/scenarios';
 import type { UserProfile } from '@/types/profile';
 import type { Scenario } from '@/types/scenarios';
+import { composeLoaders } from './utils';
+import { userProfileLoader } from './loaders';
+import { scenariosLoader } from './loaders';
 
 export interface HomeLoader {
   profile: UserProfile;
@@ -11,57 +10,10 @@ export interface HomeLoader {
   buyer: Scenario<'buyer'>[];
 }
 
-export async function homeLoader() {
-  const [profile, scenarios] = await Promise.all([
-    userProfileLoader(),
-    scenariosLoader(),
-  ]);
-
-  return { profile, ...scenarios };
-}
-
-async function scenariosLoader(): Promise<{
-  buyer: Scenario<'buyer'>[];
-  seller: Scenario<'seller'>[];
-}> {
-  try {
-    const { scenarios, setScenarios } = useScenariosStore.getState();
-
-    const [buyer, seller] = await Promise.all([
-      scenarios.buyer.length > 0
-        ? scenarios.buyer
-        : getScenarios<'buyer'>('buyer').then(items => {
-            setScenarios('buyer', items);
-            return items;
-          }),
-      scenarios.seller.length > 0
-        ? scenarios.seller
-        : getScenarios<'seller'>('seller').then(items => {
-            setScenarios('seller', items);
-            return items;
-          }),
-    ]);
-
-    return { buyer, seller };
-  } catch (error) {
-    console.error('Ошибка загрузки сценариев:', error);
-    return { buyer: [], seller: [] };
-  }
-}
-
-async function userProfileLoader(): Promise<UserProfile> {
-  const { profile, setProfile } = useUserProfileStore.getState();
-
-  if (profile !== null) {
-    return profile;
-  }
-
-  try {
-    const userProfile = await getUserProfile();
-    setProfile(userProfile);
-    return userProfile;
-  } catch (error) {
-    console.error('Ошибка загрузки профиля:', error);
-    throw error;
-  }
+export async function homeLoader(): Promise<HomeLoader> {
+  return composeLoaders({
+    profile: userProfileLoader,
+    buyer: () => scenariosLoader('buyer'),
+    seller: () => scenariosLoader('seller'),
+  });
 }
