ad9c931be61e1ee5de5d05809acd0967c6d81cc9
melon4521 <vlad123kyz@gmail.com>
2026-08-12 01:06:38 +0300

    bugfix(router): clear user and LocalStorage user data when token is expired in loader of ProtectedRoutes

--- frontend/src/hooks/auth.ts
+++ frontend/src/hooks/auth.ts
@@ -1,9 +1,10 @@
 import { useUser, useUserHydrated } from '@/store/user';
+import { isTokenExpired } from '@/utils/auth';
 
 export function useAuth() {
   const user = useUser();
   const isHydrated = useUserHydrated();
-  const isAuth = user !== null;
+  const isAuth = user !== null && !isTokenExpired(user.token);
 
   return {
     user,
