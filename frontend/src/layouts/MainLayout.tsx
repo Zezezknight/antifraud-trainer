be4e138bcbd52e11b59256a4fcb019eb75aa1fbd
melon4521 <vlad123kyz@gmail.com>
2026-08-23 18:24:59 +0300

    feat(layouts): styles in main layout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -4,9 +4,9 @@ import TopProgressBar from '@/components/TopProgressBar';
 
 function MainLayout() {
   return (
-    <div className="min-h-screen w-full bg-muted">
+    <div className="min-h-screen w-full bg-muted flex flex-col">
       <TopProgressBar />
-      <div className="flex flex-col gap-12">
+      <div className="flex flex-col gap-12 flex-1">
         <Outlet />
       </div>
     </div>
