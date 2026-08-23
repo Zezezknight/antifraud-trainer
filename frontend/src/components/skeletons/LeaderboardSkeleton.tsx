6e4d5800434f9c9986daf368916290027a2b33f0
melon4521 <vlad123kyz@gmail.com>
2026-08-24 01:33:44 +0300

    fix(components): wrap leaderboard skeleton in necessary divs

--- frontend/src/components/skeletons/LeaderboardSkeleton.tsx
+++ frontend/src/components/skeletons/LeaderboardSkeleton.tsx
@@ -2,19 +2,25 @@ import { Skeleton } from '../ui/skeleton';
 
 function LeaderboardSkeleton() {
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex items-center border-none rounded-2xl h-12">
-        <div className="w-[20%] sm:w-[15%] pl-2 flex items-center">Место</div>
-        <div className="w-[50%] sm:w-[55%] flex items-center">Пользователь</div>
-        <div className="w-[30%] flex items-center justify-end">Очки</div>
+    <div className="relative bg-background px-5 py-4 sm:px-8 sm:py-6 rounded-lg flex flex-col gap-4 sm:gap-8">
+      <h3 className="text-xl font-semibold">Таблица рейтинга</h3>
+
+      <div className="flex flex-col gap-1">
+        <div className="flex items-center border-none rounded-2xl h-12">
+          <div className="w-[20%] sm:w-[15%] pl-2 flex items-center">Место</div>
+          <div className="w-[50%] sm:w-[55%] flex items-center">
+            Пользователь
+          </div>
+          <div className="w-[30%] flex items-center justify-end">Очки</div>
+        </div>
+        <Skeleton className="w-full h-12 rounded-2xl bg-muted" />
+        <Skeleton className="w-full h-12 rounded-2xl bg-muted" />
+        <Skeleton className="w-full h-12 rounded-2xl bg-muted" />
+        <div className="w-full text-xl font-bold text-muted-foreground text-center">
+          ...
+        </div>
+        <Skeleton className="w-full h-12 rounded-2xl bg-muted" />
       </div>
-      <Skeleton className="w-full h-12 rounded-2xl bg-muted" />
-      <Skeleton className="w-full h-12 rounded-2xl bg-muted" />
-      <Skeleton className="w-full h-12 rounded-2xl bg-muted" />
-      <div className="w-full text-xl font-bold text-muted-foreground text-center">
-        ...
-      </div>
-      <Skeleton className="w-full h-12 rounded-2xl bg-muted" />
     </div>
   );
 }
